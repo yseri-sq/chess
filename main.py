b0459c30e65937de4542415a2f7f93a81db47d47
yseri-sq <yseri.sq@gmail.com>
2026-04-29 11:39:02 +0400

    update

--- main.py
+++ main.py
@@ -6,10 +6,31 @@ async def main(page : ft.Page):
     page.window.height = 650
     page.window.minimizable = False
     page.window.maximizable = False
-    page.window.always_on_top = True
+    # page.window.always_on_top = True
 
     await page.window.center()
 
+
+
+    board_view = ft.GridView(
+        width=550,
+        runs_count=8,
+        run_spacing=5,
+        controls=[]
+    )
     
+
+    def show_board():
+        cell = ft.Container(width=35, height=35, bgcolor=ft.Colors.WHITE)
+        for i in range(64):
+            board_view.controls.append(cell)
+            
+    show_board()
+
+    page.add(
+        ft.Row([
+            board_view
+        ], alignment=ft.MainAxisAlignment.CENTER)
+    )
 
 ft.run(main=main)
